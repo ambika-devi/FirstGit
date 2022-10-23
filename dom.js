94786e8f67f017d9ee184354dbc6d0973dae07a0
unknown <ambika-devi>
2022-10-23 10:59:28 +0530

    after modifications

--- dom.js
+++ dom.js
@@ -1,2 +1,9 @@
-var headerTitle=document.getElementById('header-title');
-headerTitle.style.borderBottom='solid 3px #000'
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='Hello';
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}
