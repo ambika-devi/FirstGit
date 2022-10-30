ad8d30bbf00acae6d6b712fe26db3a12ab93d8e2
ambika-devi <ambikadevithota5@gmail.com>
2022-10-30 14:42:31 +0530

    my project

---
+++ practising.js
@@ -0,0 +1,25 @@
+const studentobj = {
+
+	'yash': 26,
+
+	'vaibhav': 24,
+
+	'rajesh' : 25,
+
+	}
+    const iteratekeys=Object.keys(studentobj);
+    iteratekeys.forEach((key=> {
+    console.log(studentobj[key])
+    }))
+    
+
+    function findaverageage(studentobj){
+           return (averageage) =>{
+            return averageage=25;
+           }
+    
+    }
+    console.log(findaverageage(25));
+    
+    
+
