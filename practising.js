c6edce95e9005d5da382d35bfd5ee3e4abc23427
ambika-devi <116368772+ambika-devi@users.noreply.github.com>
2022-10-30 15:11:38 +0530

    Update practising.js

--- practising.js
+++ practising.js
@@ -1,25 +1,4 @@
-const studentobj = {
-
-	'yash': 26,
-
-	'vaibhav': 24,
-
-	'rajesh' : 25,
-
-	}
-    const iteratekeys=Object.keys(studentobj);
-    iteratekeys.forEach((key=> {
-    console.log(studentobj[key])
-    }))
-    
-
-    function findaverageage(studentobj){
-           return (averageage) =>{
-            return averageage=25;
-           }
-    
-    }
-    console.log(findaverageage(25));
+console.log('git');
     
     
 
